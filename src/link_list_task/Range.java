48df7fc9e5757e58f6e47fb9ddf4413e24055286
Oleg Chepenko <7613332@gmail.com>
2023-04-30 11:10:09 +0300

    28.04.2023

---
+++ src/link_list_task/Range.java
@@ -0,0 +1,13 @@
+package link_list_task;
+
+
+
+public class Range {
+    int low;
+    int high;
+
+    public Range(int low, int high) {
+           this.low = low;
+           this.high = high;
+    }
+}
